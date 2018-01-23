8cf93d5fefa6a2babb75e93f072f313bdcdd7941
Jonas <jard.dugarte@gmail.com>
2018-01-22 22:27:16 -0600

    Setup test databases environment

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -6,6 +6,7 @@ import { addExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
   onSubmit = (expense) => {
+    
     this.props.addExpense(expense);
     this.props.history.push('/');
   };
